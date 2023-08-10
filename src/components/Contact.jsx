a661b452eef4a74087e03be11922d34bc9c525c1
Brandon <102290201+icyde@users.noreply.github.com>
2023-08-10 13:38:04 +0800

    add form to index.html

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,10 +9,11 @@ const Contact = () => {
       {/* getform.io */}
       <form
         method="POST"
-        name="ContactForm"
+        name="contact"
         className="flex flex-col max-w-[600px] w-full"
         data-netlify="true"
       >
+        <input type="hidden" name="form-name" value="contact" />
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-[#8892b0] text-gray-300">
             Contact
